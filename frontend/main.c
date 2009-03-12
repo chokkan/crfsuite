38b8f281c7179c710d358a2e38990775ecc412bb
naoaki <naoaki@e255bd1e-7e62-11dc-b0bd-8f205bbbb416>
2009-03-12 02:22:18 +0000

    Bumped the version number.
    git-svn-id: svn+ssh://chokkan.org/home/naoaki/svnrepos/software/crfsuite/trunk@155 e255bd1e-7e62-11dc-b0bd-8f205bbbb416

--- frontend/main.c
+++ frontend/main.c
@@ -39,7 +39,7 @@
 #include "option.h"
 
 #define    APPLICATION_S    "CRFsuite"
-#define    VERSION_S        "0.7"
+#define    VERSION_S        "0.8"
 #define    COPYRIGHT_S      "Copyright (c) 2007-2009 Naoaki Okazaki"
 
 
