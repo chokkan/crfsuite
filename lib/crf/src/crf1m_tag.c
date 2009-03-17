b2abca144d08a6b955da6756edecdfd3717ceb64
naoaki <naoaki@e255bd1e-7e62-11dc-b0bd-8f205bbbb416>
2009-03-17 01:50:30 +0000

    Fixed a bug in license terms.
    git-svn-id: svn+ssh://chokkan.org/home/naoaki/svnrepos/software/crfsuite/trunk@159 e255bd1e-7e62-11dc-b0bd-8f205bbbb416

--- lib/crf/src/crf1m_tag.c
+++ lib/crf/src/crf1m_tag.c
@@ -11,10 +11,9 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Northwestern University, University of Tokyo,
- *       nor the names of its contributors may be used to endorse or promote
- *       products derived from this software without specific prior written
- *       permission.
+ *     * Neither the names of the authors nor the names of its contributors
+ *       may be used to endorse or promote products derived from this
+ *       software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
