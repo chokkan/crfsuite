36a30be4a5ed9cb4bd4b0dca537bc8f0fc865dd0
naoaki <naoaki@e255bd1e-7e62-11dc-b0bd-8f205bbbb416>
2010-07-24 15:10:25 +0000

    Experimental commit of faster exp.
    git-svn-id: svn+ssh://chokkan.org/home/naoaki/svnrepos/software/crfsuite/trunk@199 e255bd1e-7e62-11dc-b0bd-8f205bbbb416

--- lib/crf/src/vecmath.h
+++ lib/crf/src/vecmath.h
@@ -36,6 +36,65 @@
 #include <math.h>
 #include <memory.h>
 
+inline static double fastexp(double x)
+{
+    static const double MAXLOG =  7.08396418532264106224E2;     /* log 2**1022 */
+    static const double MINLOG = -7.08396418532264106224E2;     /* log 2**-1022 */
+    static const double LOG2E  =  1.4426950408889634073599;     /* 1/log(2) */
+    static const double INFINITY = 1.79769313486231570815E308;
+    static const double C1 = 6.93145751953125E-1;
+    static const double C2 = 1.42860682030941723212E-6;
+
+    int n;
+    double xx, px, qx;
+    union {
+        double d;
+        unsigned char c[8];
+	} u;
+
+    if (MAXLOG < x) {
+        return INFINITY;
+    } else if (x < MINLOG) {
+        return 0.;
+    }
+
+    n = (int)(LOG2E * x + 0.5);
+    px = (double)n;
+    x -= px * C1;
+    x -= px * C2;
+    xx = x * x;
+
+    /* px = x * P(x**2). */
+    px = 1.26177193074810590878E-4;
+    px *= xx;
+    px += 3.02994407707441961300E-2;
+    px *= xx;
+    px += 9.99999999999999999910E-1;
+    px *= x;
+
+    /* Evaluate Q(x**2). */
+    qx = 3.00198505138664455042E-6;
+    qx *= xx;
+    qx += 2.52448340349684104192E-3;
+    qx *= xx;
+    qx += 2.27265548208155028766E-1;
+    qx *= xx;
+    qx += 2.00000000000000000009E0;
+
+    /* e**x = 1 + 2x P(x**2)/( Q(x**2) - P(x**2) ) */
+    x = px / (qx - px);
+    x = 1.0 + 2.0 * x;
+
+    /* Build 2^n in double. */
+    u.d = 0;
+    n += 1023;
+    u.c[7] = (unsigned char)((n >> 4) & 0x7F);
+    u.c[6] = (unsigned char)((n & 0x0F) << 4);
+
+    return x * u.d;
+}
+
+
 inline static void veczero(floatval_t *x, const int n)
 {
     memset(x, 0, sizeof(floatval_t) * n);
@@ -135,7 +194,7 @@ inline static void vecexp(floatval_t* x, const int n)
 {
     int i;
     for (i = 0;i < n;++i) {
-        x[i] = (x[i] == 0. ? 1. : exp(x[i]));
+        x[i] = (x[i] == 0. ? 1. : fastexp(x[i]));
     }
 }
 
