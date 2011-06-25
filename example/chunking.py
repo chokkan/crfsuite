94af870352b6b24237abe60935cf1468a1103fca
Naoaki Okazaki <okazaki@chokkan.org>
2011-06-26 03:23:39 +0900

    Improved the sample script.

--- example/chunking.py
+++ example/chunking.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
 """
-An example for chunking.
+A feature extractor for chunking.
 Copyright 2010,2011 Naoaki Okazaki.
 """
 
+import sys
 import crfutils
 import template
-import sys
+
+# Separator of field values.
+separator = ' '
+
+# Field names of the input data.
+fields = 'w pos y'
 
 # Feature template. This template is identical to the one bundled in CRF++
 # distribution, but written in a Python object.
@@ -42,53 +48,4 @@ def feature_extractor(X):
         X[-1]['F'].append('__EOS__')    # EOS feature
 
 if __name__ == '__main__':
-    fi = sys.stdin
-    fo = sys.stdout
-    
-    # Each line of the input text is assumed to have a token surface,its
-    # part-of-speech, and its chunk label separated by SPACE (' ')
-    # characters in this order. An empty line presents an end of a sentence.
-    fields = ('w', 'pos', 'y')
-    
-    if len(sys.argv) < 3:
-        # The command line does not have "-m MODEL" specified.
-        # The below demonstrates how to convert an input text into the
-        # label-attribute format compatible with the CRFsuite utility
-
-        # For each sequence from STDIN.
-        for X in crfutils.readiter(fi, fields, ' '):
-            # print X   # uncomment this to see how a sequence is stored in X
-            feature_extractor(X)
-
-            # Output the labels and attributes.
-            for x in X:
-            	fo.write('%s' % x['y'])
-	        for a in x['F']:
-                    fo.write('\t%s' % a.replace(':', '__COLON__'))
-                fo.write('\n')
-            fo.write('\n')
-        
-    elif sys.argv[1] == '-m':
-        # The command line has "-m MODEL" specified.
-        # The below is an example of using the python module for tagging
-        
-        import crfsuite
-        tagger = crfsuite.Tagger()
-        tagger.open(sys.argv[2])
-
-        # For each sequence from STDIN.
-        for X in crfutils.readiter(fi, fields, ' '):
-            # Obtain features.
-            feature_extractor(X)
-            xseq = crfutils.to_crfsuite(X)
-            
-            # Tag the current sentence.
-            yseq = tagger.tag(xseq)
-            for t in range(len(X)):
-                v = X[t]
-                # Thru the input text.
-                fo.write(' '.join([v[f] for f in F]))
-                # Append the predicted label
-                fo.write(' %s\n' % yseq[t])
-            fo.write('\n')
-
+    crfutils.main(feature_extractor, fields=fields, sep=separator)
