4eb237cf81544c26c27a087fbce35a93e7584fcb
Naoaki Okazaki <okazaki@chokkan.org>
2011-07-05 12:57:04 +0900

    Updated the Python sample program.

--- swig/python/sample_tag.py
+++ swig/python/sample_tag.py
@@ -5,37 +5,51 @@ import sys
 
 def instances(fi):
     xseq = crfsuite.ItemSequence()
+
     for line in fi:
         line = line.strip('\n')
         if not line:
+            # An end of sequence.
             yield xseq
             xseq = crfsuite.ItemSequence()
             continue
 
+        # Obtain a label and attributes.
         fields = line.split('\t')
         item = crfsuite.Item()
         for field in fields[1:]:
             p = field.rfind(':')
             if p == -1:
+                # The weight of this item is not specified (weight = 1)
                 item.append(crfsuite.Attribute(field))
             else:
+                # The weight of this item is specified by ';'
                 item.append(crfsuite.Attribute(field[:p], float(field[p+1:])))
+
         xseq.append(item)
 
 if __name__ == '__main__':
     fi = sys.stdin
     fo = sys.stdout
 
+    # Create a tagger.
     tagger = crfsuite.Tagger()
+
+    # Load a model for the tagger.
     tagger.open(sys.argv[1])
 
+    # Loop for a sequence (refer to instances(fi)).
     for xseq in instances(fi):
+        # Set the sequence
         tagger.set(xseq)
-        yseq = tagger.viterbi()
-        print tagger.probability(yseq)
-        t = 0
-        for y in yseq:
-            fo.write('%s:%f\n' % (y, tagger.marginal(y, t)))
-            t += 1
-        fo.write('\n')
 
+        # Find the best label sequence (using the Viterbi algorithm).
+        yseq = tagger.viterbi()
+
+        # Show the probability of the best label sequence.
+        fo.write('%f\n' % tagger.probability(yseq))
+
+        # Output labels with marginal probabilities.
+        for t in range(len(yseq)):
+            fo.write('%s:%f\n' % (yseq[t], tagger.marginal(yseq[t], t)))
+        fo.write('\n')
--- swig/python/sample_train.py
+++ swig/python/sample_train.py
@@ -3,29 +3,38 @@
 import crfsuite
 import sys
 
+# Extend crfsuite.Trainer to receive progress messages from the training
+# algorithm (this is achieved by cross language polymorphism!)
 class Trainer(crfsuite.Trainer):
     def message(self, s):
+        # Overwrite the member function 'message' to receive messages.
         sys.stdout.write(s)
 
 def instances(fi):
     xseq = crfsuite.ItemSequence()
-    yseq = crfsuite.StringList() 
+    yseq = crfsuite.StringList()
+
     for line in fi:
         line = line.strip('\n')
         if not line:
+            # An end of sequence.
             yield xseq, tuple(yseq)
             xseq = crfsuite.ItemSequence()
             yseq = crfsuite.StringList()
             continue
 
+        # Obtain a label and attributes.
         fields = line.split('\t')
         item = crfsuite.Item()
         for field in fields[1:]:
             p = field.rfind(':')
             if p == -1:
+                # The weight of this item is not specified (weight = 1)
                 item.append(crfsuite.Attribute(field))
             else:
+                # The weight of this item is specified by ';'
                 item.append(crfsuite.Attribute(field[:p], float(field[p+1:])))
+
         xseq.append(item)
         yseq.append(fields[0])
 
@@ -33,15 +42,38 @@ if __name__ == '__main__':
     fi = sys.stdin
     fo = sys.stdout
 
-    print crfsuite.version()
+    # How to obtain CRFsuite version
+    fo.write('CRFsuite version %s\n' % crfsuite.version())
 
+    # Create an instance of a trainer.
     trainer = Trainer()
+
+    # Read training instances from STDIN (refer to instances(fi))
+    #  xseq: crfsuite.ItemSequence()
+    #  yseq: crfsuite.StringList()
+    #  0: group number
     for xseq, yseq in instances(fi):
         trainer.append(xseq, yseq, 0)
 
+    # Select a training algorithm and a graphical model.
     trainer.select('l2sgd', 'crf1d')
-    for name in trainer.params():
-        print name, trainer.get(name), trainer.help(name)
-    print trainer.get('c2')
-    trainer.train(sys.argv[1], -1)
 
+    # Show the list of parameters for the trainer.
+    fo.write('===== List of training parameters =====\n')
+    for name in trainer.params():
+        fo.write('%s=%s: %s\n' % (name, trainer.get(name), trainer.help(name)))
+    fo.write('\n')
+
+    # How to set/get a training parameter.
+    trainer.set('feature.possible_states', '1')
+    fo.write('c2 = %s\n' % trainer.get('c2'))
+    fo.write('\n')
+
+    # Start training:
+    #  the first argument: a filename of a model
+    #  the second argument: a holdout group (-1: no holdout evaluation)
+    fo.write('===== Training =====\n')
+    trainer.train(
+        sys.argv[1] if len(sys.argv) > 1 else '',
+        -1
+        )
