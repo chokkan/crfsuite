a219e0bc1a6b8a3428a8ba9d7d61d396a6b295d1
abicky <takeshi.arabiki@gmail.com>
2015-04-12 03:06:52 +0900

    Correct indentations

--- swig/python/sample_train.py
+++ swig/python/sample_train.py
@@ -17,24 +17,24 @@ def instances(fi):
     for line in fi:
         line = line.strip('\n')
         if not line:
-        	# An empty line presents an end of a sequence.
+            # An empty line presents an end of a sequence.
             yield xseq, yseq
             xseq = crfsuite.ItemSequence()
             yseq = crfsuite.StringList()
             continue
 
-		# Split the line with TAB characters.
+        # Split the line with TAB characters.
         fields = line.split('\t')
-    	
-    	# Append attributes to the item.
+        
+        # Append attributes to the item.
         item = crfsuite.Item()
         for field in fields[1:]:
             p = field.rfind(':')
             if p == -1:
-            	# Unweighted (weight=1) attribute.
+                # Unweighted (weight=1) attribute.
                 item.append(crfsuite.Attribute(field))
             else:
-            	# Weighted attribute
+                # Weighted attribute
                 item.append(crfsuite.Attribute(field[:p], float(field[p+1:])))
         
         # Append the item to the item sequence.
@@ -43,17 +43,17 @@ def instances(fi):
         yseq.append(fields[0])
 
 if __name__ == '__main__':
-	# This demonstrates how to obtain the version string of CRFsuite.
+    # This demonstrates how to obtain the version string of CRFsuite.
     print crfsuite.version()
 
-	# Create a Trainer object.
+    # Create a Trainer object.
     trainer = Trainer()
     
     # Read training instances from STDIN, and set them to trainer.
     for xseq, yseq in instances(sys.stdin):
         trainer.append(xseq, yseq, 0)
 
-	# Use L2-regularized SGD and 1st-order dyad features.
+    # Use L2-regularized SGD and 1st-order dyad features.
     trainer.select('l2sgd', 'crf1d')
     
     # This demonstrates how to list parameters and obtain their values.
