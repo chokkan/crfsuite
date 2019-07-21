chokkan/crfsuite

commit e3c42a11ec2b28ca9af3186b1d0ddb2697576355

Try aligned_alloc() and memalign() in addition to posix_memalign().

lib/crf/src/vecmath.h
@@ -46,10 +46,17 @@
 #include <stdlib.h>
 static inline void *_aligned_malloc(size_t size, size_t alignment)
 {
+#if __STDC_VERSION__ >= 201112L
+    return aligned_alloc(alignment, size);
+#elif _POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600
     void *p;
     int ret = posix_memalign(&p, alignment, size);
     return (ret == 0) ? p : 0;
+#else
+    return memalign(alignment, size);
+#endif
 }
+
 static inline void _aligned_free(void *p)
 {
     free(p);

commit 5d1bd3b803bb26582ed5cc274d6b5af6cc7f9cae

Fix macOS regression introduced in #43.

lib/crf/src/vecmath.h
@@ -48,7 +48,7 @@ static inline void *_aligned_malloc(size_t size, size_t alignment)
 {
 #if __STDC_VERSION__ >= 201112L
     return aligned_alloc(alignment, size);
-#elif _POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600
+#elif _POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600 || __APPLE__
     void *p;
     int ret = posix_memalign(&p, alignment, size);
     return (ret == 0) ? p : 0;